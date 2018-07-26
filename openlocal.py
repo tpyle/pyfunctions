45a3e5f4dd0a077b130d53fb3f78bb9b97cefa57
Thomas Pyle <thomasp162@gmail.com>
2018-07-26 16:15:25 -0400

    Fixed it so it works correctly

--- openlocal.py
+++ openlocal.py
@@ -1,8 +1,8 @@
 import os
+import sys
 import inspect
 
 def openlocal( filename, mode ):
-    return open ( os.path.join( os.path.dirname ( os.path.relpath( inspect.stack()[1][1],            
-                                                                   basePath) ), 
+    return open ( os.path.join( os.path.dirname ( os.path.relpath( inspect.getfile(sys._getframe(1))) ), 
                                 filename ), 
                   mode )
